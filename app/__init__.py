e7ea09790f15cab5ab7faf2ad7e30f4da2665bb4
Logia <carnoldcoding@gmail.com>
2024-08-02 04:46:02 -0400

    SQLAlchemy setup and folder structure for MVS architecture

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
 from flask_cors import CORS
 from app.equipment.routes import equipment
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
 
 def create_app():
+    #Load Environemntal Variables
+    load_dotenv()
 
     # Initialize Flask Instance
     app = Flask(__name__)
     CORS(app)
+
+    #Configure SQLAlchemy
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
     # Register blueprints
     app.register_blueprint(equipment)
