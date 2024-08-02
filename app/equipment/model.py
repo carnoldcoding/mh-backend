e0fbc07032a2fd35e2315c61b9917becaa04a460
Logia <carnoldcoding@gmail.com>
2024-08-02 19:17:09 -0400

    Progress on weapon model/service

--- app/equipment/model.py
+++ app/equipment/model.py
@@ -4,3 +4,23 @@ class Weapon(db.Model):
     __tablename__ = "weapon"
 
     id = db.Column(db.Integer, primary_key=True)
+    create_recipe_id = db.Column(db.Integer)
+    upgrade_recipe_id = db.Column(db.Integer)
+    previous_weapon_id = db.Column(db.Integer)
+    armorset_bonus_id = db.Column(db.Integer)
+
+    rarity = db.Column(db.Integer)
+    category = db.Column(db.Text)
+    attack = db.Column(db.Integer)
+    attack_true = db.Column(db.Integer)
+    affinity = db.Column(db.Integer)
+    defense = db.Column(db.Integer)
+
+    slot_1 = db.Column(db.Integer)
+    slot_2 = db.Column(db.Integer)
+    slot_3 = db.Column(db.Integer)
+
+    phial = db.Column(db.Text)
+    phial_power = db.Column(db.Integer)
+    shelling = db.Column(db.Text)
+    shelling_level = db.Column(db.Integer)
--- app/equipment/service.py
+++ app/equipment/service.py
@@ -2,6 +2,29 @@ from flask import jsonify
 from app.equipment.model import Weapon
 
 def get_weapons():
-    weapons = Weapon.query.limit(5).all()
-    weapon_list = [{'id': w.id } for w in weapons]
+    weapons = Weapon.query.limit(10).all()
+    weapon_list = [{
+        'id': w.id ,
+        'create_recipe_id': w.create_recipe_id,
+        'upgrade_recipe_id': w.upgrade_recipe_id,
+        'previous_weapon_id': w.previous_weapon_id,
+        'armorset_bonus_id' : w.armorset_bonus_id,
+
+        'rarity': w.rarity,
+        'category': w.category,
+        'attack': w.attack,
+        'attack_true': w.attack_true,
+        'affinity' : w.affinity,
+        'defense' : w.defense,
+        
+        'slot_1' : w.slot_1,
+        'slot_2' : w.slot_2,
+        'slot_3' : w.slot_3,
+
+        'phial' : w.phial,
+        'phial_power' : w.phial_power,
+        'shelling' : w.shelling,
+        'shelling_level' : w.shelling_level
+
+        } for w in weapons]
     return jsonify(weapon_list)
