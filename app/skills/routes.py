392955d96d24b48c630765e8699b3b14120285dc
Logia <carnoldcoding@gmail.com>
2024-08-20 19:01:11 -0400

    Refactored get_skills to take an id argument

--- app/skills/routes.py
+++ app/skills/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.skills.service import get_skill_data
 
 skill = Blueprint('skill', __name__)
 
 @skill.route('/get_skill')
 def get_skill():
-    return jsonify(get_skill_data())
+    skill_id = request.args.get('id')
+    return jsonify(get_skill_data(skill_id))
--- app/skills/service.py
+++ app/skills/service.py
@@ -3,17 +3,20 @@ from sqlalchemy import select, Table, MetaData, and_
 
 metadata = MetaData()
 
-def get_skill_data():
+def get_skill_data(skill_id : int):
     #Define Tables
     skilltree = Table('skilltree', metadata, autoload_with=db.engine)
     skilltree_text = Table('skilltree_text', metadata, autoload_with=db.engine)
     skill = Table('skill', metadata, autoload_with=db.engine)
 
     #Define Statement
-    stmt = select(skill, skilltree, skilltree_text)\
+    stmt = select(skill.c.description,
+                  skill.c.level,
+                   skilltree, 
+                   skilltree_text.c.name)\
         .select_from(skill.join(skilltree, skill.c.skilltree_id == skilltree.c.id)
                      .join(skilltree_text, skilltree.c.id == skilltree_text.c.id))\
-        .where(and_(skill.c.lang_id == "en", skilltree_text.c.lang_id == "en"))\
+        .where(and_(skill.c.lang_id == "en", skilltree_text.c.lang_id == "en", skill.c.skilltree_id == skill_id))\
         .limit(10)
     
     #Execute, Parse and Return
